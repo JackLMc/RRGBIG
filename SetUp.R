2dc2a0ec8b705b658560bf15ec6a5953986f89c8
JackLMc <jacklukemcmurray@gmail.com>
2022-05-24 19:22:01 +0100

    The folders

---
+++ SetUp.R
@@ -0,0 +1,12 @@
+# Course structure
+# create the directories that we need for the course
+dir.create("rnaseq_course")
+
+dir.create("rnaseq_course/singularity_image")
+dir.create("rnaseq_course/raw_data")
+dir.create("rnaseq_course/quality_control")
+dir.create("rnaseq_course/trimming")
+dir.create("rnaseq_course/reference_genome")
+dir.create("rnaseq_course/mapping")
+dir.create("rnaseq_course/differential_expression")
+dir.create("rnaseq_course/functional_analysis")
